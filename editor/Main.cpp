ed40d07d95219d195917434dea6fed4b61d90658
Gustav <sir.gustav.the.coder@gmail.com>
2009-09-29 19:09:10 +0000

    polygon & intellizoom

--- editor/Main.cpp
+++ editor/Main.cpp
@@ -4,6 +4,7 @@
 ////////////////////////////////////////////////////////////
 #include "wxSFMLCanvas.hpp"
 #include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
 #include <boost/foreach.hpp>
 #include <iostream>
 
@@ -21,19 +22,47 @@
 /// Custom SFML canvas
 ////////////////////////////////////////////////////////////
 
+class MyPolygon
+{
+public:
+	void draw(sf::RenderTarget* target, sf::Vector2f* last)
+	{
+		const std::size_t size = positions.size();
+		if( size < 1 ) return;
+
+		sf::Vector2f first = positions[0];
+		sf::Vector2f second = first;
+
+		for(std::size_t index=1; index < size; ++index)
+		{
+			const sf::Vector2f second = positions[index];
+			target->Draw(sf::Shape::Line(first.x, first.y, second.x, second.y, 1, sf::Color(0,0,0)));
+			first = second;
+		}
+
+		second = last ? *last : positions[0];
+		target->Draw(sf::Shape::Line(first.x, first.y, second.x, second.y, 1, sf::Color(0,0,0)));
+
+		glEnd();
+	}
+
+	bool isValid() const
+	{
+		return positions.size() > 2;
+	}
+
+	std::vector<sf::Vector2f> positions;
+};
+
 class MyCanvas : public wxSFMLCanvas
 {
 public :
-
-	////////////////////////////////////////////////////////////
-	/// Construct the canvas
-	///
-	////////////////////////////////////////////////////////////
 	MyCanvas(wxWindow* parent, wxWindowID id, const wxPoint& position, const wxSize& size, long style = 0)
 		: wxSFMLCanvas(parent, id, position, size, style)
-		, firstPlaced(false)
+		, zoom(0)
 	{
 		Connect(wxEVT_MOTION, wxMouseEventHandler(MyCanvas::OnMouseMove));
+		Connect(wxEVT_MOUSEWHEEL, wxMouseEventHandler(MyCanvas::OnMouseMove));
 
 		view = GetDefaultView();
 	}
@@ -42,61 +71,97 @@ private :
 	virtual void OnUpdate()
 	{
 		static bool previous = false;
-		const bool down = GetInput().IsMouseButtonDown(sf::Mouse::Left);
+		const sf::Input& Input = GetInput();
+		const bool down = Input.IsMouseButtonDown(sf::Mouse::Left);
+		const bool move = Input.IsMouseButtonDown(sf::Mouse::Middle);
 
-		const bool move = GetInput().IsMouseButtonDown(sf::Mouse::Middle);
+		static bool previousAbort = false;
+		const bool abortDown = Input.IsKeyDown(sf::Key::Escape);
+		const bool abort = abortDown && !previousAbort;
+		previousAbort = abortDown;
+		
+		static bool previousAccept = false;
+		const bool acceptDown = Input.IsKeyDown(sf::Key::Return);
+		const bool accept = acceptDown && !previousAccept;
+		previousAccept = acceptDown;
 
 		if( move )
 		{
 			view.Move(-diff);
 		}
 
+		if( zoom != 0 ) {
+			const sf::Vector2f oc = view.GetCenter();
+			const sf::Vector2f omp = ConvertCoords(pos.x, pos.y);
+			view.SetCenter(mp);
+			view.Zoom(1.0f + 0.2 * zoom );
+			const sf::Vector2f nc = view.GetCenter();
+			const sf::Vector2f nmp = ConvertCoords(pos.x, pos.y);
+			view.SetCenter( nmp - (omp-oc) );
+
+			/*view.SetCenter(
+
+			viewPortCenter = new PointF(
+				viewPortCenter.X + ( (e.X - hafWidth) /(2* Zoom)),
+				viewPortCenter.Y + ((e.Y - (panel1.Height/2)) / (2*Zoom)));*/
+
+		}
+
 		if( down && !previous )
 		{
-			firstPlaced = !firstPlaced;
-
-			if( firstPlaced == false ) 
-			{
-				lines.push_back(sf::Shape::Line(first.x, first.y, mp.x, mp.y, 1, sf::Color(0,0,0)));
-			}
-
-			first = mp;
+			current.positions.push_back(mp);
+			
 		}
 		previous = down;
 
+		if( abort ) current.positions.resize(0);
+		if( accept )
+		{
+			if( current.isValid() )
+			{
+				polygons.push_back(current);
+			}
+			current = MyPolygon();
+		}
+
 		diff = sf::Vector2f(0,0);
+		zoom = 0;
 
 		SetView(view);
 
 		// Clear the view
 		Clear(sf::Color(255, 255, 255));
-		if( firstPlaced )
+
+		BOOST_FOREACH(MyPolygon& shape, polygons)
 		{
-			sf::Shape temp =  sf::Shape::Line(first.x, first.y, mp.x, mp.y, 1, sf::Color(0,0,0));
-			Draw(temp);
+			shape.draw(this, 0);
 		}
 
-		BOOST_FOREACH(sf::Shape& shape, lines)
-		{
-			Draw(shape);
-		}
+		current.draw(this, &mp);
 	}
 
 	void OnMouseMove(wxMouseEvent& event)
 	{
-		const sf::Vector2f pos = sf::Vector2f(event.GetX(), event.GetY());
+		pos = sf::Vector2f(event.GetX(), event.GetY());
 		static sf::Vector2f old = pos;
 		mp = ConvertCoords(pos.x, pos.y);
 		diff = mp - ConvertCoords(old.x, old.y);
 		old = pos;
+		if( event.m_wheelRotation != 0 )
+		{
+			zoom -= event.m_wheelRotation / static_cast<float>(event.m_wheelDelta);
+		}
 	}
+
+	sf::Vector2f pos;
 
 	sf::Vector2f mp;
 	sf::Vector2f diff;
-	sf::Vector2f first;
 	sf::View view;
-	bool firstPlaced;
-	std::vector<sf::Shape> lines;
+	float zoom;
+
+	MyPolygon current;
+	std::vector<MyPolygon> polygons;
 };
 
 
