7a140b7d718c650994d32f2575184fc6f9b03cf6
Gustav <sir.gustav.the.coder@gmail.com>
2009-10-04 08:45:34 +0000

    glu tesselator seems to work better and compile faster than gcal

--- editor/Main.cpp
+++ editor/Main.cpp
@@ -8,11 +8,8 @@
 #include <boost/foreach.hpp>
 #include <iostream>
 #include <boost/shared_ptr.hpp>
-
-#include <CGAL/basic.h>
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Partition_traits_2.h>
-#include <CGAL/partition_2.h>
+#include <list>
+#include <sstream>
 
 #ifdef _DEBUG
 #define OPTIONS "-s-d"
@@ -24,6 +21,221 @@
 #pragma comment(lib, "sfml-system" OPTIONS ".lib")
 #pragma comment(lib, "sfml-window" OPTIONS ".lib")
 
+// -------------------
+// Tesselation code
+
+// http://www.flipcode.com/articles/article_tesselating.shtml
+
+
+void CALLBACK  tcbCombine (GLdouble c[3], void *d[4], GLfloat w[4], void **out, void* data);
+
+GLvoid CALLBACK  onError(GLenum err) {
+	std::string str = (char *) gluErrorString(err);
+}
+struct VectorData {
+	VectorData(double ax, double ay, double az)
+		: x(ax)
+		, y(ay)
+		, z(az)
+	{
+	}
+	double x;
+	double y;
+	double z;
+};
+struct PolygonData {
+	PolygonData(const VectorData& a, const VectorData& b, const VectorData& c)
+	{
+		points.push_back(a);
+		points.push_back(b);
+		points.push_back(c);
+	}
+	std::vector<VectorData> points;
+};
+
+void CALLBACK  tcbBegin (GLenum prim, void *data);
+void CALLBACK  tcbVertex (void *vertex, void *data);
+void CALLBACK  tcbEnd (void *data);
+
+bool IsOdd(std::size_t x)
+{
+	return (x % 2) == 1;
+}
+
+class Tesselator {
+public:
+	Tesselator() {
+		mTesselatorObject = gluNewTess();
+
+		gluTessCallback (mTesselatorObject, GLU_TESS_BEGIN_DATA, reinterpret_cast<void (__stdcall *)(void)>(tcbBegin));
+		gluTessCallback (mTesselatorObject, GLU_TESS_VERTEX_DATA, reinterpret_cast<void (__stdcall *)(void)>(tcbVertex));
+		gluTessCallback (mTesselatorObject, GLU_TESS_END_DATA, reinterpret_cast<void (__stdcall *)(void)>(tcbEnd) );
+		gluTessCallback (mTesselatorObject, GLU_TESS_COMBINE_DATA, reinterpret_cast<void (__stdcall *)(void)>(tcbCombine));
+		
+		//gluTessCallback(mTesselatorObject, GLU_ERROR,			reinterpret_cast<void (__stdcall *)(void)>(&onError) );
+	}
+	virtual ~Tesselator() {
+		gluDeleteTess(mTesselatorObject);
+	}
+
+	void setWindingRule(unsigned int pRule) {
+		gluTessProperty(mTesselatorObject, GLU_TESS_WINDING_RULE, pRule);
+	}
+	void setBoundary(bool pBoundary) {
+		gluTessProperty(mTesselatorObject, GLU_TESS_BOUNDARY_ONLY, pBoundary);
+	}
+
+	void contour_begin() {
+		gluTessBeginContour(mTesselatorObject);
+	}
+	void vertex(double x, double y, double z) {
+		double* ptr = newVector(x, y, z);
+		gluTessVertex (mTesselatorObject, ptr, ptr);
+	}
+	void contour_end() {
+		gluTessEndContour(mTesselatorObject);
+	}
+
+	void polygon_begin() {
+		gluTessBeginPolygon(mTesselatorObject, this);
+	}
+	void polygon_end() {
+		gluTessEndPolygon(mTesselatorObject);
+	}
+
+	double* newVector(double x, double y, double z) {
+		VectorData vec(x,y,z);
+		data.push_back( vec );
+		return & (*(--data.end())).x;
+	}
+
+	// internal
+	void onBegin(GLenum prim)
+	{
+		currentState = prim;
+	}
+	void onVertex(double* vertex)
+	{
+		vectors.push_back(VectorData(vertex[0], vertex[1], vertex[2]) );
+	}
+	void onEnd()
+	{
+		const std::size_t size = vectors.size();
+		std::vector<PolygonData> polygons;
+
+		switch(currentState)
+		{
+		case GL_TRIANGLES:
+			if( (size % 3) != 0 ) throw "wtf";
+			for(std::size_t i=0; i<size; i+=3)
+			{
+				polygons.push_back(PolygonData(vectors[i], vectors[i+1], vectors[i+2]));
+			}
+			break;
+		case GL_TRIANGLE_STRIP:
+			for(std::size_t i=0;i<size-2; ++i)
+			{
+				if( IsOdd(i) ) polygons.push_back(PolygonData(vectors[i], vectors[i+1], vectors[i+2]));
+				else polygons.push_back(PolygonData(vectors[i+1], vectors[i], vectors[i+2]));
+			}
+			break;
+		case GL_TRIANGLE_FAN:
+			for(std::size_t i=0;i<size-2; ++i)
+			{
+				polygons.push_back(PolygonData(vectors[0], vectors[i+1], vectors[i+2]));
+			}
+			break;
+		default:
+			throw "not supported";
+		}
+
+		onPolygons(polygons);
+		vectors.clear();
+	}
+
+protected:
+	virtual void onPolygons(const std::vector<PolygonData>& polygons) = 0;
+private:
+	// tesselation data
+	GLUtesselator* mTesselatorObject;
+	std::list<VectorData> data;
+	
+	// polygon compilation data
+	std::vector<VectorData> vectors;
+	GLenum currentState;
+};
+
+void CALLBACK  tcbBegin (GLenum prim, void *data){
+	Tesselator* owner = (Tesselator*) data;
+	owner->onBegin(prim);
+}
+
+void CALLBACK  tcbVertex (void *vertex, void *data) {
+	Tesselator* owner = (Tesselator*) data;
+	owner->onVertex((GLdouble *)vertex);
+}
+void CALLBACK  tcbEnd (void *data) {
+	Tesselator* owner = (Tesselator*) data;
+	owner->onEnd();
+}
+void CALLBACK  tcbCombine (GLdouble c[3], void *d[4], GLfloat w[4], void **out, void* data){
+	Tesselator* owner = (Tesselator*) data;
+	*out = owner->newVector(c[0], c[1], c[2]);
+}
+
+void DrawOutline(sf::RenderTarget* target, double thickness, const sf::Color& color, const std::vector<sf::Vector2f>& positions, sf::Vector2f* last)
+{
+	const std::size_t size = positions.size();
+	if( size < 1 ) return;
+
+	sf::Vector2f first = positions[0];
+	sf::Vector2f second = first;
+
+	for(std::size_t index=1; index < size; ++index)
+	{
+		const sf::Vector2f second = positions[index];
+		target->Draw(sf::Shape::Line(first.x, first.y, second.x, second.y, thickness, color));
+		first = second;
+	}
+
+	second = last ? *last : positions[0];
+	target->Draw(sf::Shape::Line(first.x, first.y, second.x, second.y, thickness, color));
+}
+
+struct TesselatePolygonRaii
+{
+	Tesselator* me;
+
+	TesselatePolygonRaii(Tesselator* t)
+		: me(t)
+	{
+		me->polygon_begin();
+	}
+
+	~TesselatePolygonRaii()
+	{
+		me->polygon_end();
+	}
+};
+
+struct TesselateContourRaii
+{
+	Tesselator* me;
+
+	TesselateContourRaii(Tesselator* t)
+		: me(t)
+	{
+		me->contour_begin();
+	}
+
+	~TesselateContourRaii()
+	{
+		me->contour_end();
+	}
+};
+
+//--------------------
+
 class Tri
 {
 public:
@@ -32,11 +244,14 @@ public:
 
 	void compile()
 	{
+		std::reverse(points.begin(), points.end());
 		shape = sf::Shape();
 		BOOST_FOREACH(const sf::Vector2f& p, points)
 		{
-			shape.AddPoint(p, sf::Color(0, 255, 0) );
+			shape.AddPoint(p, sf::Color(0, 255, 0), sf::Color(0,0,0));
 		}
+		//shape.EnableOutline(true);
+		//shape.SetOutlineWidth(1);
 	}
 };
 
@@ -60,6 +275,37 @@ public:
 			target->Draw(t.shape);
 		}
 	}
+
+	void debug(sf::RenderTarget* target)
+	{
+		BOOST_FOREACH(const Tri& t, tris)
+		{
+			DrawOutline(target, 1, sf::Color(255, 0,0), t.points, 0);
+		}
+	}
+};
+
+class MyTesselator : public Tesselator
+{
+public:
+	TriList* tris;
+	MyTesselator()
+		: tris(0)
+	{
+	}
+protected:
+	virtual void onPolygons(const std::vector<PolygonData>& polygons)
+	{
+		BOOST_FOREACH(const PolygonData& p, polygons)
+		{
+			Tri t;
+			BOOST_FOREACH(const VectorData& data, p.points)
+			{
+				t.points.push_back(sf::Vector2f(data.x, data.y));
+			}
+			tris->tris.push_back(t);
+		}
+	}
 };
 
 class MyPolygon
@@ -67,23 +313,9 @@ class MyPolygon
 public:
 	void draw(sf::RenderTarget* target, sf::Vector2f* last)
 	{
-		const std::size_t size = positions.size();
-		if( size < 1 ) return;
-
 		if( tris ) tris->draw(target);
-
-		sf::Vector2f first = positions[0];
-		sf::Vector2f second = first;
-
-		for(std::size_t index=1; index < size; ++index)
-		{
-			const sf::Vector2f second = positions[index];
-			target->Draw(sf::Shape::Line(first.x, first.y, second.x, second.y, 1, sf::Color(0,0,0)));
-			first = second;
-		}
-
-		second = last ? *last : positions[0];
-		target->Draw(sf::Shape::Line(first.x, first.y, second.x, second.y, 1, sf::Color(0,0,0)));
+		DrawOutline(target, 3, sf::Color(0,0,0), positions, last);
+		if( tris ) tris->debug(target);
 	}
 
 	bool isValid() const
@@ -93,55 +325,30 @@ public:
 
 	void compile()
 	{
-		typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-		typedef CGAL::Partition_traits_2<K>                         Traits;
-		typedef Traits::Point_2                                     Point_2;
-		typedef Traits::Polygon_2                                   Polygon_2;
-		typedef std::list<Polygon_2>                                Polygon_list;
-		typedef CGAL::Creator_uniform_2<int, Point_2>               Creator;
-
-		Polygon_2    polygon;
-		Polygon_list partition_polys;
-
-		BOOST_FOREACH(const sf::Vector2f& pos, positions)
-		{
-			polygon.push_back(Point_2(pos.x, pos.y));
-		}
-
-		/*if( orientation_2(polygon.begin(), polygon.end(), Traits) == COUNTERCLOCKWISE )
-		{
-			std::reverse(polygon.begin(), polygon.end());
-		}*/
-
-		CGAL::y_monotone_partition_2(polygon.vertices_begin(),
-			polygon.vertices_end(),
-			std::back_inserter(partition_polys));
-
 		if( tris ) tris->tris.resize(0);
 		else tris.reset( new TriList() );
 
-		BOOST_FOREACH(const Polygon_2& poly, partition_polys)
-		{
-			Tri tri;
-			int i = 0;
-			for(Polygon_2::Vertex_iterator p = poly.vertices_begin(); p != poly.vertices_end(); ++p)
-			{
-				tri.points.push_back( sf::Vector2f(p->x(), p->y()) );
-			}
-			/*for(int i=0; i<3; ++i)
-			{
-				tri.points[i] = sf::Vector2f(poly[i].x(), poly[i].y());
-			}*/
-			tris->tris.push_back(tri);
-		}
+		MyTesselator tesselator;
+		tesselator.tris = tris.get();
 
+		{
+			TesselatePolygonRaii polygon(&tesselator);
+			TesselateContourRaii contour(&tesselator);
+
+			BOOST_FOREACH(const sf::Vector2f& pos, positions)
+			{
+				tesselator.vertex(pos.x, pos.y, 0);
+			}
+		}
+		
 		tris->compile();
 	}
 
 	std::vector<sf::Vector2f> positions;
-	std::vector<sf::Shape> shapes;
 	boost::shared_ptr<TriList> tris;
 };
+
+
 
 class MyCanvas : public wxSFMLCanvas
 {
@@ -168,7 +375,7 @@ private :
 		const bool abortDown = Input.IsKeyDown(sf::Key::Escape);
 		const bool abort = abortDown && !previousAbort;
 		previousAbort = abortDown;
-		
+
 		static bool previousAccept = false;
 		const bool acceptDown = Input.IsKeyDown(sf::Key::Return);
 		const bool accept = acceptDown && !previousAccept;
@@ -191,15 +398,15 @@ private :
 			/*view.SetCenter(
 
 			viewPortCenter = new PointF(
-				viewPortCenter.X + ( (e.X - hafWidth) /(2* Zoom)),
-				viewPortCenter.Y + ((e.Y - (panel1.Height/2)) / (2*Zoom)));*/
+			viewPortCenter.X + ( (e.X - hafWidth) /(2* Zoom)),
+			viewPortCenter.Y + ((e.Y - (panel1.Height/2)) / (2*Zoom)));*/
 
 		}
 
 		if( down && !previous )
 		{
 			current.positions.push_back(mp);
-			
+
 		}
 		previous = down;
 
