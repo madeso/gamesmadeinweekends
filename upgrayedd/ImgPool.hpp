66e8d35bbe70706cdeb6ca15ac66ba9daddd333e
Gustav <sir.gustav.the.coder@gmail.com>
2010-03-05 07:03:14 +0000

    added a sprite class that wraps the sf::Sprite

--- upgrayedd/ImgPool.hpp
+++ upgrayedd/ImgPool.hpp
@@ -1,14 +1,11 @@
 #ifndef UPGRAYEDD_IMG_POOL_HPP
 #define UPGRAYEDD_IMG_POOL_HPP
 
-#include <SFML/Graphics/Image.hpp>
-#include <boost/shared_ptr.hpp>
+#include "Img.hpp"
 #include <string>
 
 namespace upgrayedd
 {
-	typedef boost::shared_ptr<sf::Image> Img;
-
 	// todo: replace with an actual pool..
 	Img Load(const std::string& path);
 }
