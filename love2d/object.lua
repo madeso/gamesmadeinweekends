ff07469b54226a11d82971b5ec3ccc9de0ab93ce
sir gustav the coder <sir.gustav.the.coder@gmail.com>
2011-12-15 23:39:37 +0100

    added tileset

--- love2d/object.lua
+++ love2d/object.lua
@@ -1,18 +1,21 @@
 require "oo"
 vector = require "hump/vector"
+require "tileset"
 
 class "Object"
 {
-	pos = vector(40,40)
+	pos = vector(40,40);
+	index = 1;
 }
 
 function Object:__init(img, x,y)
 	self.pos = vector(x,y)
-	self.img = love.graphics.newImage(img)
+	self.img = Tileset:new(img, 16)
 end
 
 function Object:draw()
-	love.graphics.draw(self.img, self.pos:unpack())
+	self.img:draw(self.index, self.pos:unpack())
+	--love.graphics.draw(self.img, self.pos:unpack())
 end
 
 function Object:update(dt)
