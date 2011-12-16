210ea483614ce331389eff76aaf5d2019e4d5319
sir gustav the coder <sir.gustav.the.coder@gmail.com>
2011-12-16 09:33:51 +0100

    sample player movement

--- love2d/player.lua
+++ love2d/player.lua
@@ -4,10 +4,14 @@ vector = require "hump/vector"
 require "tilesets"
 
 
-class "Player" {}
+class "Player"
+{
+	speed = 30
+}
 
 function Player:__init(x,y)
 	self.super = Object:new("ninja.png", x,y)
+	self.velocity = vector(0,0)
 end
 
 function Player:draw()
@@ -15,5 +19,18 @@ function Player:draw()
 end
 
 function Player:update(dt)
-	self.super.pos = self.super.pos + vector(30,30)*dt
+	local m = vector(0,0)
+	if love.keyboard.isDown("left") then
+		m = m - vector(1,0)
+	end
+	if love.keyboard.isDown("right") then
+		m = m + vector(1,0)
+	end
+	if love.keyboard.isDown("up") then
+		m = m - vector(0,1)
+	end
+	if love.keyboard.isDown("down") then
+		m = m + vector(0,1)
+	end
+	self.super.pos = self.super.pos + m*self.speed*dt
 end
