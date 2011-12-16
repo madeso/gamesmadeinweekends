eb089448f493a864a5da45178cf5dab3c430b450
sir gustav the coder <sir.gustav.the.coder@gmail.com>
2011-12-16 17:42:41 +0100

    camera focus

--- love2d/main.lua
+++ love2d/main.lua
@@ -9,7 +9,7 @@ function love.load()
 	world = World:new("level.tmx")
 	tilesets:add("ninja.png", 16)
 	world:add(Object:new("ninja.png", 100, 100, 70))
-	world:add(Player:new(400, 200))
+	world:add(Player:new(world:getCamera(), 400, 200))
 end
 
 function love.draw()
