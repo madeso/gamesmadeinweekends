762501fe43c347201dca78872fe0527d0088f16c
sir gustav the coder <sir.gustav.the.coder@gmail.com>
2012-03-07 14:29:44 +0100

    collidable, world setting for hiding collision boxes among other stuff

--- love2d/player.lua
+++ love2d/player.lua
@@ -1,51 +1,19 @@
-require "object"
+require "collidable"
 Class = require 'hump.class'
 vector = require "hump.vector"
 require "tilesets"
 require "console"
 
-Player = Class{inherits=Object, function(self, camera, x,y)
-	Object.construct(self, "ninja.png", x,y)
+Player = Class{inherits=Collidable, function(self, camera, x,y)
+	Collidable.construct(self, "ninja.png", x,y, 16,16)
 	self.speed = 90
 	self.velocity = vector(0,0)
 	self.camera = camera
 end}
 
-function Player:enterWorld(world, c)
-	self.col = c:addRectangle(self.pos.x,self.pos.y,16,16)
-	self.col.type = self
-end
-
-function Player:draw()
-	self:Object_draw()
-	self.col:draw("fill")
-	local mx, my = self.camera:getWorldFromView(love.mouse.getPosition())
-	--love.graphics.line(self.pos.x, self.pos.y, mx, my)
-end
-
-function Player:onCollision(other, mx, my)
-	if other == nil then
-		print("adjusting", mx, my)
-		self.col:move(0, my)
-		self.col:move(mx, 0)
-	end
-end
-
-function Player:move(xy)
-	self._move_x = xy.x
-	self._move_y = xy.y
-end
-
-function Player:_apply_hor()
-	self.col:move(vector(self._move_x, 0))
-end
-
-function Player:_apply_ver()
-	self.col:move(vector(0, self._move_y))
-end
 
 function Player:update(dt)
-	self.pos.x, self.pos.y = self.col:center()
+	self:Collidable_update(dt)
 	local m = vector(0,0)
 	if love.keyboard.isDown("left") then
 		m = m - vector(1,0)
@@ -59,7 +27,6 @@ function Player:update(dt)
 	if love.keyboard.isDown("down") then
 		m = m + vector(0,1)
 	end
-	--self.pos = self.pos + 
 	self:move(m*self.speed*dt)
 end
 
