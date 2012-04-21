4a2f2789d435db08c9108c31a0679bcc78bacb21
sir gustav the coder <sir.gustav.the.coder@gmail.com>
2012-04-21 17:02:51 +0200

    first commit of my ld23 game

---
+++ ludumdare_23-tiny_world/tilesets.lua
@@ -0,0 +1,20 @@
+require "tileset"
+Class = require 'hump.class'
+
+Tilesets = Class{name="Tilesets", function(self)
+	self.sets = {}
+end}
+
+function Tilesets:add(img, size)
+	local set = Tileset(img, size)
+	self.sets[img] = set
+	return set
+end
+
+function Tilesets:get(img)
+	local set = self.sets[img]
+	assert(set)
+	return set
+end
+
+tilesets = Tilesets()
