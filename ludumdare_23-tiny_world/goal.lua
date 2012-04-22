bc3c1c6a90b1a4ba2eb5003466111bdd0bf70c31
sir gustav the coder <sir.gustav.the.coder@gmail.com>
2012-04-23 01:36:38 +0200

    added goal item and started on gamestates

---
+++ ludumdare_23-tiny_world/goal.lua
@@ -0,0 +1,22 @@
+require "collidable"
+Class = require 'hump.class'
+vector = require "hump.vector"
+require "tilesets"
+require "console"
+require 'iolib'
+require "player.lua"
+require "debug.lua"
+
+Goal = Class{inherits=Collidable, name="Goal", function(self, x,y)
+	Collidable.construct(self, "ninja.png", x,y, 40,25,-10,36)
+	self:setAnimation({25})
+end}
+
+function Goal:colon_with(other, world)
+	return false
+end
+
+function Goal:update(dt)
+	self:Collidable_update(dt)
+	self:setPassive()
+end
